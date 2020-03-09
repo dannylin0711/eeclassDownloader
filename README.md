720fad09930ca537d597fa055430ff9add362c67
dannylin0711 <dannylin0711@gmail.com>
2020-03-09 10:36:27 +0800

    Update README

--- README.md
+++ README.md
@@ -3,8 +3,13 @@
 ## Usage
 First compile using your favorite compiler, then execute the executable as
 ```
-    <Executable_Name> <EECLASS_FolderName> <Save Location Name>
+    <Executable_Name> <EECLASS_URL> <Save Location Name>
 ```
 
 The program won't work if any argument is missing.
 Default save location is under the executable directory.
+
+eeclass url example:
+```
+    http://eeclass.utaipei.edu.tw/sysdata/doc/<hash_prefix>/<hash>/images/
+```
--- main.c
+++ main.c
@@ -6,15 +6,27 @@ int main(int argc, char *argv[]){
     printf("%d",argc);
     if (argc == 3){
         FILE *p;
-        p = fopen("temp.dat","wb+");
+        p = fopen("temp.txt","w+");
         for(int i = 1; i < 200; i++){
+            printf("Output: %d\n",i);
             fprintf(p,"%s%d.jpg\n",argv[1],i);
         }
-        char buf[1024] = "wget -i temp.dat -P ./";
+        char buf[1024] = "wget -i temp.txt -P ./";
         strcat(buf,argv[2]);
         fclose(p);
         system(buf);
-        system("rm temp.dat");
+        system("rm temp.txt");
+    }else if (argc == 2){
+        FILE *p;
+        p = fopen("temp.txt","w+");
+        for(int i = 1; i < 200; i++){
+            printf("Output: %d\n",i);
+            fprintf(p,"%s%d.jpg\n",argv[1],i);
+        }
+        char buf[1024] = "wget -i temp.txt -P ./";
+        fclose(p);
+        system(buf);
+        system("rm temp.txt");
     }
     return 0;
 }
